87b9b4ae6f9fa8f4ae85eac0b3c76be195662be5
unknown <lukas.tomsu@seznam.cz>
2018-07-04 16:44:52 +0200

    update

--- preprocessing.py
+++ preprocessing.py
@@ -9,6 +9,7 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
 
+'''
 # Missing data
 from sklearn.preprocessing import Imputer
 
@@ -27,11 +28,16 @@ X = onehotencoder.fit_transform(X).toarray()
 
 label_encoder_Y = LabelEncoder() 
 Y= label_encoder_Y.fit_transform(Y) 
+'''
 
 # Splitting the dataset into the Training and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
+'''
 # Feature scaling
 from sklearn.preprocessing import StandardScaler
-sc_X
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+'''
