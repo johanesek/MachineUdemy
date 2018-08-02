de49bcf3213ffab2371000497ce1acb87874c17f
unknown <lukas.tomsu@seznam.cz>
2018-08-02 16:31:36 +0200

    added NLP

--- Upper Confidence Bound (UCB)/upper_confidence_bound.py
+++ Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -12,13 +12,14 @@ N = 10000
 ads_selected = []
 numbers_of_selections = [0] * d #creates vector of 0 long d
 sums_of_rewards = [0] * d
+total_reward = 0
 for n in range(0, N):
     ad = 0
     max_upper_bound = 0
     for i in range(0, d):
         if(numbers_of_selections[i]>0):
             average_reward = sums_of_rewards[i]/numbers_of_selections[i]
-            delta_i = math.sqrt(3/2*math.log(n + 1)/numbers_of_selections)
+            delta_i = math.sqrt(3/2*math.log(n + 1)/numbers_of_selections[i])
             upper_bound = average_reward + delta_i
         else:
             upper_bound = 1e400
@@ -27,4 +28,11 @@ for n in range(0, N):
             ad = i
     ads_selected.append(ad)
     numbers_of_selections[ad] += 1
-    sums_of_rewards[ad] = sums_of_rewards[ad] + dataset[n, ad] 
+    sums_of_rewards[ad] = sums_of_rewards[ad] + dataset.values[n, ad]
+    total_reward = total_reward + dataset.values[n, ad]
+
+print(total_reward)
+print(sums_of_rewards)
+
+plt.hist(ads_selected)
+plt.show()
